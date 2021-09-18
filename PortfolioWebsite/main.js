39f0b8e1dfed1612a83f9c88eb6df629cf21adb3
User <bridibusher@gmail.com>
2021-09-17 21:16:00 -0700

    Added template code

--- PortfolioWebsite/main.js
+++ PortfolioWebsite/main.js
@@ -1,6 +1,48 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import * as THREE from 'three';
+
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+
+const renderer = new THREE.WebGL1Renderer({
+  canvas: document.querySelector('#bg')
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10,3,16,100)
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347, wireframe:true});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const AmbientLight = new THREE.AmbientLight(0xffffff);
+AmbientLight.position.set(20,20,20)
+
+scene.add(AmbientLight)
+
+
+
+
+
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate()
+
